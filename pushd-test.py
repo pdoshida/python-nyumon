189d9ff759b33cd9e18a9745fc2fc4977777d08d
oshida <five52113@gmail.com>
2018-12-01 16:52:13 +0900

    pushd-test.py 完了.

--- pushd-test.py
+++ pushd-test.py
@@ -17,30 +17,27 @@ def main():
 
     # <h3 class="restaurant__name">  に該当するものを取り出す
     # <p class="restaurant__voice">  に該当するものを取り出す
-    participation_tables_n = soup.find_all('h3', class_='restaurant__name')
-    participation_tables_v = soup.find_all('p', class_='restaurant__voice')
-    print(participation_tables_n)
-    print(participation_tables_v)
-    print()
+    rest_n = soup.find_all('h3', class_='restaurant__name')
+    rest_v = soup.find_all('p', class_='restaurant__voice')
+#    print(rest_n)
+#    print()
+#    print(rest_v)
+#    print()
 
-    # participation_tables を順番に見て、"講師・メンター枠"の情報を取り出す
-#    for participation_table in participation_tables:
-        # <table><thead><tr><th> に該当するタグの要素を取り出す (参加者枠の種類が記載されているので)
-#        print(participation_table)
+    new_rset_n = []
+    for name in rest_n:
+        #print(name.get_text())
+        new_rset_n.append(name.get_text())
+    
+    new_rset_v = []
+    for vname in rest_v:
+        #print(vname.get_text())
+        new_rset_v.append(name.get_text())
 
-#        # 参加者枠を示す文字に "講師・メンター枠" が含まれるものを取り出す
-#        if '講師・メンター枠' in participant_type:
-#            mentors_table = participation_table
-#            break
-#
-#    # 講師・メンター枠の HTML の中で class=display_name に該当するものを取り出す
-#    # mentor_names は List
-#    mentor_names = mentors_table.find_all(class_='display_name')
-#
-#    # 取り出した 講師・メンター枠の要素から純粋な名前だけを取り出す(前後の無駄な空行や改行などを取り除く)
-#    for mentor_name in mentor_names:
-#        print(mentor_name.get_text().strip())
+    #print(zip(new_rset_n, new_rset_v))
 
+    for n, v in zip(new_rset_n, new_rset_v):
+        print('{} {}'.format(n, v))
 
 if __name__ == '__main__':
     main()
